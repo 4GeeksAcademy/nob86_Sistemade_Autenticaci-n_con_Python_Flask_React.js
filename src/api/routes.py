9c191f3721bec67d108e2e5d6cbe1adea6ec59f7
nob86 <gustavoadallacroce@gmail.com>
2024-10-30 15:59:19 +0000

    despues sigo

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,11 +5,32 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+
+
+# Crea una ruta para autenticar a los usuarios y devolver el token JWT
+# La función create_access_token() se utiliza para generar el JWT
+@api.route("/login", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    # Consulta la base de datos por el nombre de usuario y la contraseña
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        # el usuario no se encontró en la base de datos
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    # Crea un nuevo token con el id de usuario dentro
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
 
 
 @api.route('/hello', methods=['POST', 'GET'])
