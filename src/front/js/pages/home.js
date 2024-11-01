63f7680b7d09ce3df7a1f726a2c55afd2a2f779e
nob86 <gustavoadallacroce@gmail.com>
2024-11-01 16:24:09 +0000

    frontend

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,18 +4,33 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			
-			<p>
-				
-			</p>
-			<div className="alert alert-info">
-				
-			</div>
-			
-		</div>
-	);
+  return (
+    <div>
+      <div class="input-group input-group-lg pt-5">
+        <span class="input-group-text" id="inputGroup-sizing-lg">
+          E-mail
+        </span>
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        ></input>
+      </div>
+
+      <div class="input-group input-group-lg pt-5">
+        <span class="input-group-text" id="inputGroup-sizing-lg">
+          Password
+        </span>
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        ></input>
+      </div>
+    </div>
+  );
 };
