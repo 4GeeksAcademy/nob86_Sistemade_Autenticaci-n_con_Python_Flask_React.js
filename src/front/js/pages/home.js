7435f7b1a674dd39e5aedd68ea2abc86a0f24125
nob86 <gustavoadallacroce@gmail.com>
2024-11-01 17:28:47 +0000

    despues sigo

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,28 +8,38 @@ export const Home = () => {
 
   return (
     <div>
-      <div class="input-group input-group-lg pt-5">
-        <span class="input-group-text" id="inputGroup-sizing-lg">
-          E-mail
-        </span>
-        <input
-          type="text"
-          class="form-control"
-          aria-label="Sizing example input"
-          aria-describedby="inputGroup-sizing-lg"
-        ></input>
-      </div>
-
-      <div class="input-group input-group-lg pt-5">
-        <span class="input-group-text" id="inputGroup-sizing-lg">
-          Password
-        </span>
-        <input
-          type="text"
-          class="form-control"
-          aria-label="Sizing example input"
-          aria-describedby="inputGroup-sizing-lg"
-        ></input>
+      <div className="">
+      <form>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label pt-5">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="your e-mail adress here"
+          ></input>
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Your password here"
+          ></input>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
       </div>
     </div>
   );
