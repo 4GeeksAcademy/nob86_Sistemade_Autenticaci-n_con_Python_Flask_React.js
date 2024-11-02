f7931edbea4df52d9bffbd4cdc78187b2569e398
nob86 <gustavoadallacroce@gmail.com>
2024-11-02 00:07:54 +0000

    ver que falta

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,79 +1,80 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
-		username: null,
-		password: null
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+      token: null,
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-			login: async (username, password) => {
-				const resp = await fetch(`https://urban-chainsaw-g455rxp7x6vg29jxj-3001.app.github.dev/`, { 
-					 method: "POST",
-					 headers: { "Content-Type": "application/json" },
-					 body: JSON.stringify({ username, password }) 
-				})
-		   
-				if(!resp.ok) throw Error("There was a problem in the login request")
-		   
-				if(resp.status === 401){
-					 throw("Invalid credentials")
-				}
-				else if(resp.status === 400){
-					 throw ("Invalid email or password format")
-				}
-				const data = await resp.json()
-				// Guarda el token en la localStorage
-				// También deberías almacenar el usuario en la store utilizando la función setItem
-				localStorage.setItem("jwt-token", data.token);
-		   
-				return data
-		   },
+      login: async (username, password) => {
+        const resp = await fetch(
+          `https://urban-chainsaw-g455rxp7x6vg29jxj-3001.app.github.dev/`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
+          }
+        );
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+        if (!resp.ok) throw Error("There was a problem in the login request");
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        if (resp.status === 401) {
+          throw "Invalid credentials";
+        } else if (resp.status === 400) {
+          throw "Invalid email or password format";
+        }
+        const data = await resp.json();
+        // Guarda el token en la localStorage
+        // También deberías almacenar el usuario en la store utilizando la función setItem
+        localStorage.setItem("jwt-token", data.token);
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        return data;
+      },
+
+      getMessage: async () => {
+        try {
+          // fetching data from the backend
+          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+          const data = await resp.json();
+          setStore({ message: data.message });
+          // don't forget to return something, that is how the async resolves
+          return data;
+        } catch (error) {
+          console.log("Error loading message from backend", error);
+        }
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
